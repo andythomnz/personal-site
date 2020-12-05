40c6bb575f11bc76aab410efd553203c4835cc57
Andrew Thompson <andythomnz@gmail.com>
2020-12-06 09:55:34 +1300

    Added my AWS CCP certification

--- app/data/resume/certifications.js
+++ app/data/resume/certifications.js
@@ -8,8 +8,8 @@ const certifications = [{
   date: 'July 2020',
   content: '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="a443807d-1268-4b27-b737-428b52008d03" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
 }, {
-  date: 'August 2020',
-  content: '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="60615e70-6409-4752-9d77-3553a43d13d2" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
+  date: 'September 2020',
+  content: '<div data-iframe-width="150" data-iframe-height="270" data-share-badge-id="5854b0eb-10e1-4360-be6f-197deecac1ea" data-share-badge-host="https://www.youracclaim.com"></div><script type="text/javascript" async src="//cdn.youracclaim.com/assets/utilities/embed.js"></script>',
 },
 ];
 
